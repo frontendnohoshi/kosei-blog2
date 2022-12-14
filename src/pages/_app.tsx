80a902c9875f9eb9829bbc06bb7b73d87d5e6fe6
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-14 23:36:16 +0900

    幅調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-auto">
       <header className="bg-slate-900 text-white">
-        <div className="container mx-auto flex flex-col pt-5 px-5">
+        <div className="container mx-auto flex flex-col px-5 pt-5 max-w-7xl">
           <div className="flex items-center">
             <Link href={"/"} passHref>
               <a className="mb-3 font-medium sm:mb-0">
                 <h1 className="text-4xl">Powerlifting Life</h1>
               </a>
             </Link>
-            <form action="#" className="ml-auto">
+            <form action="#" className="ml-auto mr-5">
               <label className="">
                 <input type="checkbox" />
                 <span className=""></span>
@@ -21,11 +21,11 @@ function MyApp({ Component, pageProps }: AppProps) {
             </form>
           </div>
           <p className="mb-3 text-center text-sm text-red-600 sm:text-left">
-            PowerLifting Life is the best way to understand the Life.
+            PowerLifting Life is the best way to understand your Life.
           </p>
         </div>
       </header>
-      <main className="container mx-auto mt-8">
+      <main className="container mx-auto mt-8 max-w-7xl">
         <Component {...pageProps} />
       </main>
     </div>
