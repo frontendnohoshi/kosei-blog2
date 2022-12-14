9aad97689db40a0f004b02ee83f977f7938fc647
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-14 20:00:32 +0900

    ヘッダーの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,28 @@ import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto max-w-prose">
-      <header className="border-b border-gray-300 py-8">
-        <h1>
-          <Link href="/">
-            <a className="text-5xl font-bold">こーせーのブログ</a>
-          </Link>
-        </h1>
+    <div className="mx-auto">
+      <header className="bg-slate-900 text-white">
+        <div className="container mx-auto flex flex-col pt-5 px-5">
+          <div className="flex items-center">
+            <Link href={"/"} passHref>
+              <a className="mb-3 font-medium sm:mb-0">
+                <h1 className="text-4xl">Powerlifting Life</h1>
+              </a>
+            </Link>
+            <form action="#" className="ml-auto">
+              <label className="">
+                <input type="checkbox" />
+                <span className=""></span>
+              </label>
+            </form>
+          </div>
+          <p className="mb-3 text-center text-sm text-red-600 sm:text-left">
+            PowerLifting Life is the best way to understand the Life.
+          </p>
+        </div>
       </header>
-      <main className="mt-8">
+      <main className="container mx-auto mt-8">
         <Component {...pageProps} />
       </main>
     </div>
