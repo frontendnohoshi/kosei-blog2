7572263f5d82af382b821c9c23e5c384670d4415
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-14 23:36:51 +0900

    UI構築中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import { MicroCMSListResponse } from "microcms-js-sdk";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+import { MicroCMSContentId, MicroCMSDate, MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
 import { ComponentProps, useState } from "react";
 import { client } from "src/libs/client";
+import { Blog, Tag } from "src/types/blog";
 
-export type Blog = {
-  title: string;
-  body: string;
-};
-
-type Props = MicroCMSListResponse<Blog>;
+type Props = { blogs: MicroCMSListResponse<Blog>; tags: MicroCMSListResponse<Tag> };
 
 const Home: NextPage<Props> = (props) => {
-  const [search, setSearch] = useState<Props>();
+  console.log(props);
+
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
 
   const handleSubmit: ComponentProps<"form">["onSubmit"] = async (event) => {
     event.preventDefault();
@@ -22,7 +22,7 @@ const Home: NextPage<Props> = (props) => {
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({ q }),
     });
-    const json: Props = await data.json();
+    const json: MicroCMSListResponse<Blog> = await data.json();
     setSearch(json);
     console.log(search);
   };
@@ -31,37 +31,68 @@ const Home: NextPage<Props> = (props) => {
     setSearch(undefined);
   };
 
-  const contents = search ? search.contents : props.contents;
-  const totalCount = search ? search.totalCount : props.totalCount;
+  const contents = search ? search.contents : props.blogs.contents;
+  const totalCount = search ? search.totalCount : props.blogs.totalCount;
 
   return (
     <div>
-      <form className="flex gap-x-2" onSubmit={handleSubmit}>
-        <input type="text" name="query" className="border border-black px-2" />
-        <button className="border border-black px-2">検索</button>
-        <button type="reset" onClick={handleReset} className="border border-black px-2">
+      <form className="flex justify-center gap-x-2" onSubmit={handleSubmit}>
+        <input type="text" name="query" className="rounded border border-black px-2" />
+        <button className="text-md rounded border border-black px-2">検索</button>
+        <button type="reset" className="text-md rounded border border-black px-2" onClick={handleReset}>
           リセット
         </button>
       </form>
-      <p className="mt-4 text-gray-400">{`${search ? "検索結果" : "記事の総数"}: ${totalCount}件`}</p>
-      <ul className="mt-4 space-y-4">
-        {contents.map((content) => {
-          return (
-            <li key={content.id}>
-              <Link href={`/blog/${content.id}`}>
-                <a className="text-xl text-blue-800 underline hover:text-blue-400">{content.title}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <p className="mt-4 ml-5 text-gray-400">{`${search ? "検索結果" : "記事の総数"}: ${totalCount}件`}</p>
+      <div className="mx-5 mt-4 flex flex-col justify-between sm:flex-row">
+        <ul className=" w-full">
+          {contents.map((content) => {
+            return (
+              <li className="sm:r-5 mb-5 mr-0 flex flex-col border-b sm:mr-5" key={content.id}>
+                <h2 className="text-xl font-bold">{content.title}</h2>
+                <div className="mt-3">
+                  <ul className="text-md flex items-center gap-x-1">
+                    {content.tags.map((tag) => {
+                      return (
+                        <li className="rounded-full bg-slate-200 px-2" key={tag.id}>
+                          #{tag.tag}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <time dateTime={content.publishedAt} className="mt-1 block text-xs text-gray-500">
+                    {dayjs(content.publishedAt).locale("ja").format("YYYY年MM月DD日(ddd) HH時mm分")}
+                  </time>
+                </div>
+                <Link href={`/blog/${content.id}`}>
+                  <a>
+                    <button className="mt-3 mb-5 rounded-xl bg-red-600 p-4 text-sm text-white duration-500 hover:bg-slate-900">
+                      READ MORE {">>"}
+                    </button>
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="mb-5">
+          <h3 className="text-md rouded-full  bg-red-600 px-3 py-1 font-bold text-white w-full sm:w-60">Tags</h3>
+          <ul>
+            {[...props.tags.contents].reverse().map((tag) => {
+              return <li key={tag.id}> {tag.tag}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await client.getList<Blog>({ endpoint: "blog" });
-  return { props: data };
+export const getStaticProps: GetStaticProps = async () => {
+  const blogs = await client.getList({ endpoint: "blog" });
+  const tags = await client.getList({ endpoint: "tag" });
+
+  return { props: { blogs, tags } };
 };
 
 export default Home;
