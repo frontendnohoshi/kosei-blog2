f9200efc93b6b2562636bb44436c8a33f68d0c68
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-14 23:37:40 +0900

    微調整

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { client } from "src/libs/client";
-import { Blog } from "..";
+import { Blog } from "src/types/blog";
 
 type Props = Blog & MicroCMSContentId & MicroCMSDate;
 
@@ -11,8 +11,13 @@ const BlogId: NextPage<Props> = (props) => {
     <div>
       <h1 className="text-4xl font-bold">{props.title}</h1>
       <time dateTime={props.publishedAt} className="mt-2 block">
-        {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
+        {dayjs(props.publishedAt).format("YYYY年MM月DD日 HH時mm分")}
       </time>
+      <div className="mt-2 flex">
+        {props.tags.map((tag) => {
+          return <div className="mr-1" key={tag.id}>{tag.tag}</div>;
+        })}
+      </div>
       <article className="prose prose-sm mt-8" dangerouslySetInnerHTML={{ __html: props.body }} />
     </div>
   );
