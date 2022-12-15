64eac7eed52a5e27af069d74a22fcab4939290e6
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-15 19:39:19 +0900

    コミットする時は必ずisDebugをfalseにする

--- src/components/utils/Debugger.ts
+++ src/components/utils/Debugger.ts
@@ -1,4 +1,4 @@
-const isDebug = true;
+const isDebug = false;
 
 export const getDebugger = (enabled: boolean = true): ((message?: any, ...optionalParams: any[]) => void) => {
   return isDebug && enabled ? console.log.bind(console) : () => {};
