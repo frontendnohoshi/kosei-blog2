f0c95c4de4361f623e61641654592716ef3c227b
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-17 09:26:26 +0900

    consol.log削除

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getDebugger } from "src/components/utils/Debugger";
 import { client } from "src/libs/client";
 import { Blog } from "src/types/blog";
 
+const debug = getDebugger(true);
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body.q);
   const data = await client.getList<Blog>({ endpoint: "blog", queries: { q: req.body.q } });
-  console.log(data);
   // TODO try catchで書く
   res.status(200).json(data);
 };
