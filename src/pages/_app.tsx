19959cf834a5de88fd51efd71898e18d8f57ee38
xiahwasenahoshi <78987138+xiahwasenahoshi@users.noreply.github.com>
2022-12-17 09:26:58 +0900

    Headタグ導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { getDebugger } from "src/components/utils/Debugger";
 import Header from "src/components/Header";
 import Footer from "src/components/Footer";
+import Head from "next/head";
 
 const debug = getDebugger(true);
 
@@ -11,6 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div className="flex min-h-screen flex-col dark:bg-black dark:text-slate-100">
+      <Head>
+        <title>KOSEI BLOG</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
